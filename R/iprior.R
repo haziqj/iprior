ff4d26ef37ac0e6ae7b9a78ac6eec8f40d5bad5d
Haziq Jamil <haziqjamil@users.noreply.github.com>
2015-12-07 12:55:30 +0000

    Changing back to require

--- R/iprior.R
+++ R/iprior.R
@@ -1,9 +1,4 @@
-### Library packages
-library(Matrix)			#to create diagonal matrices
-library(MASS)			#to sample from MVN dist.
-library(mvtnorm)
-library(Brobdingnag)
-library(numDeriv)
+
 
 ## classic Fisher cats data from package MASS
 data(cats, package="MASS")
@@ -30,6 +25,12 @@ fn.H2a <- function(x, y=NA){ #takes in vector of covariates
 ###
 
 ipriorEM <- function(x, y, maxit=50000, delt=0.00001, report.int=1000, silent=F){
+	### Library packages
+	require(Matrix)			#to create diagonal matrices
+	require(MASS)			#to sample from MVN dist.
+	require(mvtnorm)
+	require(numDeriv)
+	
 	X <- x
 	Y <- y
 	N <- length(Y)
