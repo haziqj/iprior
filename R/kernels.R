a1feecc0e6c0349809ad45bfe01506604c791f1a
Haziq Jamil <haziqjamil@users.noreply.github.com>
2015-12-15 20:54:01 +0000

    New update: Multiple lambdas
    This update now supports the use of multiple lambdas, specifically one
    for each covariate used. This is the default option. If require to set
    all lambdas equal, then call iprior with option `one.lam=T`

--- R/kernels.R
+++ R/kernels.R
@@ -14,3 +14,18 @@ fn.H2a <- function(x, y=NA){ #takes in vector of covariates
 	tmp <- tcrossprod(y-xbar, x-xbar)
 	tmp
 }
+
+## Pearson kernel
+fn.H1 <- function(x, y=NA){	#takes the group vector as argument
+	if(is.na(sum(y))) y <- x
+	z <- c(x,y)
+	N3 <- length(z)
+	n1 <- as.data.frame(table(x))[,2]
+	n2 <- as.data.frame(table(y))[,2]
+	prop <- as.data.frame(table(z))[,2]/N3
+	
+	indexi <- c(0,cumsum(n2)); indexj <- c(0,cumsum(n1))
+	tmp <- matrix(-1, nr=sum(n2), nc=sum(n1))
+	for(k in 1:min(length(n1),length(n2))) tmp[(indexi[k]+1):indexi[k+1],(indexj[k]+1):indexj[k+1]] <- matrix(1/prop[k]-1, nr=n2[k], nc=n1[k])
+	tmp
+}
