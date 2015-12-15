70fad42a944b5472b2fd965a5fdec23fce63ed45
Haziq Jamil <haziqjamil@Haziqs-MacBook-Pro.local>
2015-12-15 20:54:01 +0000

    New update: Multiple lambdas
    This update now supports the use of multiple lambdas, specifically one
    for each covariate used. This is the default option. If require to set
    all lambdas equal, then call iprior with option `one.lam=T`

--- R/iprior.R
+++ R/iprior.R
@@ -1,98 +1,3 @@
-## Canonical kernel function
-fn.H2 <- function(x, y=NA){ #takes in vector of covariates
-	if(is.na(sum(y))) y <- x
-	z <- c(x,y)
-	tmp <- tcrossprod(y, x)
-	tmp
-}
-
-## (centred) Canonical kernel function
-fn.H2a <- function(x, y=NA){ #takes in vector of covariates
-	if(is.na(sum(y))) y <- x
-	z <- c(x,y)
-	xbar <- mean(z)
-	tmp <- tcrossprod(y-xbar, x-xbar)
-	tmp
-}
-
-###
-### EM ALGORITHM
-###
-
-ipriorEM <- function(x, y, maxit=50000, delt=0.00001, report.int=1000, silent=F){
-	### Library packages
-	require(Matrix, quietly=T)			#to create diagonal matrices
-	require(MASS, quietly=T)			#to sample from MVN dist.
-	require(mvtnorm, quietly=T)
-	require(numDeriv, quietly=T)
-
-	X <- x
-	Y <- y
-	N <- length(Y)
-	p <- ncol(X)
-	x0 <- rep(1, N)
-	lambda <- abs(rnorm(1))
-	alpha <- rnorm(1)
-	psi <- abs(rnorm(1))
-	
-	### Define the kernel matrix
-	H.mat <- 0
-	for(j in 1:p){
-		H.mat <- H.mat + fn.H2a(X[,j]) 
-	}
-	H.matsq <- H.mat %*% H.mat
-
-	Var.Y <- lambda * lambda * psi * H.matsq + (1/psi) * diag(N)
-	Var.Y.inv <- chol2inv(chol(Var.Y))
-	#Var.Y.inv <- solve(Var.Y)
-	log.lik0 <- dmvnorm(Y-alpha, rep(0,N), Var.Y, log=T)
-	if(!silent) cat("START iter", 0, log.lik0, "\n")
-	log.lik1 <- log.lik0 + 2*delt
-	i <- 0
-	
-	while((i != maxit) && (abs(log.lik0 - log.lik1) > delt)){
-	
-	i <- i + 1
-	log.lik0 <- log.lik1
-	
-	### Estimating alpha
-	tmp.alpha <- crossprod(x0, Var.Y.inv)
-	alpha <- as.vector(tcrossprod(Y, tmp.alpha) / tcrossprod(x0, tmp.alpha))
-	
-	### Estimating lambda using EM
-	w.hat <- psi * lambda * H.mat %*% (Var.Y.inv %*% matrix(Y - alpha, nc=1))
-	Var.w.hat <- Var.Y.inv + w.hat %*% t(w.hat)
-	T1 <- sum(H.matsq * Var.w.hat)
-	T2 <- crossprod(Y-alpha, crossprod(H.mat, w.hat))
-	lambda <- as.vector(T2/T1)
-	
-	### Estimating psi using EM	
-	Var.Y <- lambda * lambda * psi * H.matsq + (1/psi) * diag(N)
-	Var.Y.inv <- chol2inv(chol(Var.Y))
-	w.hat <- psi * lambda * H.mat %*% (Var.Y.inv %*% matrix(Y - alpha, nc=1))
-	Var.w.hat <- Var.Y.inv + w.hat %*% t(w.hat)
-	# T4 <- tr(Var.Y.inv) + crossprod(w.hat)
-	# psi <- as.vector(T4/N)
-	T3 <- crossprod(Y-alpha) + lambda^2 * sum(H.matsq * Var.w.hat) - 2*lambda * crossprod(Y-alpha, crossprod(H.mat, w.hat))
-	psi <- as.vector(N/T3)
-
-	### New value of log-likelihood
-	Var.Y <- lambda * lambda * psi * H.matsq + (1/psi) * diag(N)
-	log.lik1 <- dmvnorm(Y-alpha, rep(0,N), Var.Y, log=T)
-	check <- i %% report.int
-	if( !is.na(check) && check==0 && !silent ){
-		if(log.lik1 >= log.lik0) cat("INCREASE iter", i, log.lik1, "\n") 
-		else cat("DECREASE iter", i, log.lik1, "\n")
-	}
-	
-	}
-
-	if(!silent) cat("EM complete.\n", "\nNumber of iterations =", i, "\n")
-	if(!silent) cat("Log-likelihood = ", log.lik1, "\n")
-	
-	list(alpha=alpha, lambda=lambda, psi=psi, log.lik=log.lik1, no.iter=i, H.mat=H.mat, H.matsq=H.matsq)
-}
-
 ###
 ### The generic function is a standard R function with a special body, usually containing 
 ### only a call to UseMethod
@@ -101,21 +6,31 @@ ipriorEM <- function(x, y, maxit=50000, delt=0.00001, report.int=1000, silent=F)
 iprior <- function(x, y, ...) UseMethod("iprior")
 
 ## The default method
-iprior.default <- function(x, y, maxit=50000, delt=0.00001, report.int=1000, silent=F, ...){
+iprior.default <- function(x, y, one.lam=F, maxit=50000, delt=0.001, report.int=100, silent=F, ...){
 	x <- as.matrix(x)
 	y <- as.numeric(y)
 	n <- length(y)
 	
-	est <- ipriorEM(x, y, maxit=maxit, delt=delt, report.int=report.int, silent=silent)
-	param <- c(est$alpha, est$lambda, est$psi)
-	names(param) <- c("alpha", "lambda", "psi")
-	
+	if(!one.lam){
+		est <- ipriorEM2(x, y, maxit=maxit, delt=delt, report.int=report.int, silent=silent)
+		param <- c(est$alpha, est$lambda, est$psi)
+		names(param) <- c("alpha", paste0("lambda", 1:length(est$lambda)), "psi")
+		H.mat.lam <- Reduce('+', mapply('*', est$H.mat, est$lambda, SIMPLIFY=F))
+	}
+	if(one.lam){
+		est <- ipriorEM1(x, y, maxit=maxit, delt=delt, report.int=report.int, silent=silent)
+		param <- c(est$alpha, est$lambda, est$psi)
+		names(param) <- c("alpha", "lambda", "psi")		
+		H.mat.lam <- est$lambda * est$H.mat
+	}
+
 	##calculate fitted values
-	Var.Y <- est$lambda * est$lambda * est$psi * est$H.matsq + (1/est$psi) * diag(n)
-	Var.Y.inv <- solve(Var.Y)
-	w.hat <- (est$psi * est$lambda) * est$H.mat %*% (Var.Y.inv %*% matrix(y - est$alpha, nc=1))
-	Var.w.hat <- Var.Y.inv + tcrossprod(w.hat)
-	Y.hat <- est$alpha + est$lambda * as.vector(crossprod(est$H.mat, w.hat))
+	H.mat.lamsq <- H.mat.lam %*% H.mat.lam	
+	Var.Y <- est$psi*H.mat.lamsq + (1/est$psi) * diag(n)
+	Var.Y.inv <- solve(Var.Y)	
+	w.hat <- est$psi*H.mat.lam %*% (Var.Y.inv %*% matrix(y - est$alpha, nc=1))
+	W.hat <- Var.Y.inv + tcrossprod(w.hat)
+	Y.hat <- est$alpha + as.vector(crossprod(H.mat.lam, w.hat))
 	
 	est$call <- match.call()
 	est$coefficients <- param
@@ -123,6 +38,7 @@ iprior.default <- function(x, y, maxit=50000, delt=0.00001, report.int=1000, sil
 	est$w.hat <- w.hat
 	est$yval <- y
 	est$xval <- x
+	est$one.lam <- one.lam
 	
 	class(est) <- "iprior"
 	est
@@ -143,7 +59,10 @@ summary.iprior <- function(object, ...){
 		alpha <- theta[1]; lambda <- theta[-c(1,length(theta))]; psi <- theta[length(theta)]
 		n <- length(fitted(object))
 		y <- object$yval
-		Var.Y <- lambda * lambda * psi * object$H.matsq + (1/psi) * diag(n)
+		if(!object$one.lam) H.mat.lam <- Reduce('+', mapply('*', object$H.mat, lambda, SIMPLIFY=F))
+		if(object$one.lam) H.mat.lam <- lambda * object$H.mat
+		H.mat.lamsq <- H.mat.lam %*% H.mat.lam	
+		Var.Y <- psi*H.mat.lamsq + (1/psi) * diag(n)
 		loglik <- dmvnorm(y-alpha, rep(0,n), Var.Y, log=T)
 		loglik
 	}
@@ -180,37 +99,47 @@ iprior.formula <- function(formula, data=list(), ...){
 	x <- model.matrix(attr(mf, "terms"), data=mf)
 	y <- model.response(mf)
 	
-	est <- iprior.default(x, y, ...)
+	est <- iprior(x, y, ...)
 	est$call <- match.call()
 	est$formula <- formula
 	names(est$fitted.values) <- row.names(mf)
+	est$terms <- attr(mf, "terms")
 	est
 }
 
 ## Prediction
 predict.iprior <- function(object, newdata=NULL, ...){
+	Y <- object$y; X <- object$x; p <- ncol(X)
 	if(is.null(newdata)) ystar <- fitted(object)
 	else{
 		if(!is.null(object$formula)){
 			## model has been fitted using formula interface
-			xstar <- model.matrix(object$formula, newdata)
+			tt <- terms(object)
+			Terms <- delete.response(tt)
+			m <- model.frame(Terms, newdata)
+			xstar <- model.matrix(Terms, m)
+			# xstar <- model.matrix(object$formula, newdata)
 			xcolnames <- colnames(xstar); xrownames <- rownames(xstar)
-			wheres.int <- (colnames(xstar) == "(Intercept)")
-			xstar <- xstar[,!wheres.int]
-			x <- as.matrix(xstar)
-			#colnames(xstar) <- xnames[!wheres.int]
+			# wheres.int <- (colnames(xstar) == "(Intercept)")
+			# xstar <- matrix(xstar[,!wheres.int], nc=p)
+			# colnames(xstar) <- xcolnames[!wheres.int]
 		}
 		else{
 			xstar <- as.matrix(newdata)
 		}
 		
 		## Define new kernel matrix
-		Y <- object$y; X <- object$x; p <- ncol(X)
-		H.mat <- 0
-		for(j in 1:p){
-			H.mat <- H.mat + fn.H2a(X[,j], xstar) 
+		if(!object$one.lam){
+			H.mat <- NULL
+			for(j in 1:p) H.mat[[j]] <- fn.H2a(X[,j], xstar[,j]) 
+			H.mat.lam <- Reduce('+', mapply('*', H.mat, object$lambda, SIMPLIFY=F))
 		}
-		ystar <- as.vector(object$alpha + object$lambda * (H.mat %*% object$w.hat))
+		if(object$one.lam){
+			H.mat <- 0
+			for(j in 1:p) H.mat <- H.mat + fn.H2a(X[,j], xstar[,j]) 
+			H.mat.lam <- object$lambda * H.mat
+		}
+		ystar <- as.vector(object$alpha + (H.mat.lam %*% object$w.hat))
 		names(ystar) <- xrownames
 	}
 	ystar
