3507d2752a2d8074aa3ffe3d1aa0ebfcf4366fa8
Haziq Jamil <haziqjamil@users.noreply.github.com>
2015-12-16 19:56:34 +0000

    Pearson kernels are here!
    At least a beta version of it is. The iprior function now recognises
    categorical variables in the dataframe (as factors) and applied the
    Pearson kernel appropriately. All other variables are still applied the
    Canonical kernel.
    
    This commit fixes issues #2, and then some. More (hopefully useful)
    information is added into the summary.

--- R/kernels.R
+++ R/kernels.R
@@ -1,14 +1,16 @@
 ## Canonical kernel function
-fn.H2 <- function(x, y=NA){ #takes in vector of covariates
-	if(is.na(sum(y))) y <- x
+fn.H2 <- function(x, y=NULL){ #takes in vector of covariates
+	if(is.null(y)) y <- x
 	z <- c(x,y)
 	tmp <- tcrossprod(y, x)
 	tmp
 }
 
 ## (centred) Canonical kernel function
-fn.H2a <- function(x, y=NA){ #takes in vector of covariates
-	if(is.na(sum(y))) y <- x
+fn.H2a <- function(x, y=NULL){ #takes in vector of covariates
+	x <- as.numeric(x)
+	if(is.null(y)) y <- x
+	else y <- as.numeric(y)
 	z <- c(x,y)
 	xbar <- mean(z)
 	tmp <- tcrossprod(y-xbar, x-xbar)
@@ -16,16 +18,21 @@ fn.H2a <- function(x, y=NA){ #takes in vector of covariates
 }
 
 ## Pearson kernel
-fn.H1 <- function(x, y=NA){	#takes the group vector as argument
-	if(is.na(sum(y))) y <- x
+fn.H1 <- function(x, y=NULL){	#takes the group vector as argument
+	x <- as.numeric(x)
+	if(is.null(y)) y <- x
+	else y <- as.numeric(y)
 	z <- c(x,y)
 	N3 <- length(z)
 	n1 <- as.data.frame(table(x))[,2]
 	n2 <- as.data.frame(table(y))[,2]
-	prop <- as.data.frame(table(z))[,2]/N3
+	prop <- table(z)/N3
 	
-	indexi <- c(0,cumsum(n2)); indexj <- c(0,cumsum(n1))
-	tmp <- matrix(-1, nr=sum(n2), nc=sum(n1))
-	for(k in 1:min(length(n1),length(n2))) tmp[(indexi[k]+1):indexi[k+1],(indexj[k]+1):indexj[k+1]] <- matrix(1/prop[k]-1, nr=n2[k], nc=n1[k])
+	tmp <- matrix(NA, nr=sum(n2), nc=sum(n1))
+	for(i in 1:sum(n2)){
+		for(j in 1:sum(n1)){
+			tmp[i,j] <- as.numeric(y[i] == x[j]) / prop[names(prop) == y[i]] - 1
+		}
+	}
 	tmp
 }
