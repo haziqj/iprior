6c832a8d99251cc8df6df451f1135e74fbeaeaaf
Haziq Jamil <haziqjamil@users.noreply.github.com>
2015-12-17 22:39:01 +0000

    Further enhancements to Pearson kernel
    Previous version was wrong for several reason:
    1. The Pearson kernel function `fn.H1` did not correctly concatenate
    factors. Must use `unlist(list(factor1, factor2))` instead of `c`. As a
    result, the input of `fn.H1(x,y)` was incorrect, but `fn.H1(x)` was OK.
    2. The `iprior.predict` function did not use the correct kernel
    function, so was not able to produce the correct kernel for prediction.
    This is fixed.
    
    Moreover, the Pearson kernel function was re-written and now is
    massively faster than the previous version.

--- R/kernels.R
+++ R/kernels.R
@@ -18,21 +18,68 @@ fn.H2a <- function(x, y=NULL){ #takes in vector of covariates
 }
 
 ## Pearson kernel
-fn.H1 <- function(x, y=NULL){	#takes the group vector as argument
-	x <- as.numeric(x)
-	if(is.null(y)) y <- x
-	else y <- as.numeric(y)
-	z <- c(x,y)
-	N3 <- length(z)
-	n1 <- as.data.frame(table(x))[,2]
-	n2 <- as.data.frame(table(y))[,2]
-	prop <- table(z)/N3
+fn.H1 <- function(x, y=NULL){ #takes in vectors of type factors
 	
-	tmp <- matrix(NA, nr=sum(n2), nc=sum(n1))
-	for(i in 1:sum(n2)){
-		for(j in 1:sum(n1)){
-			tmp[i,j] <- as.numeric(y[i] == x[j]) / prop[names(prop) == y[i]] - 1
-		}
+	fn.index <- function(k, tmp){
+  		w <- tmp[[k]]
+ 		cbind(
+    	row = rep(w, times=length(w):1 ) ,
+    	col = unlist(lapply(1:length(w), function(x) c(NA,w)[-(0:x)])))
 	}
-	tmp
+	
+	ytmp <- y
+	if(is.null(ytmp)) y <- x
+	z <- unlist(list(x,y)); z <- as.numeric(z)
+	x <- z[1:length(x)]; y <- z[(length(x)+1):length(z)]
+	prop <- table(z)/length(z)
+	
+	tmpx <- unique(lapply(sort(x), function(k) which(x == k)))	
+	indexx <- lapply(1:length(unique(x)), fn.index, tmp=tmpx)
+	nx <- length(unique(x))
+	prop[names(prop) > nx] <- Inf
+	indexx[[nx+1]] <- cbind(row=1:length(x), col=1:length(x))
+	if(is.null(ytmp)) indexy <- indexx
+	else{
+		tmpy <- unique(lapply(sort(y), function(k) which(y == k)))
+		indexy <- lapply(1:length(unique(y)), fn.index, tmp=tmpy)	
+	}
+	unqy <- sort(unique(y))
+
+	tmp.mat <- matrix(-1, nr=length(y), nc=length(x))
+	for(i in 1:length(unqy)){
+		rowind <- indexy[[i]][,1]
+		colind <- indexx[[ min(unqy[i], nx+1) ]][,2]
+		tmp.mat[rowind, colind] <- 1 / prop[ unqy[i] ] - 1
+	}
+	tmp.mat
 }
+
+### PEARSON KERNEL TEST
+### n=100,1000,2000,...,10000
+### with roughly 10% different factors. i.e. n=2000 has 200 unique factors
+# N <- c(seq(100, 900, 100), seq(1000, 10000, 1000))
+# res <- NULL
+# for(n in N){
+	# x <- sample(1:(n/10), n, T)
+	# t1 <- system.time(fn.H1(x))[3]
+	# t2 <- system.time(fn.H1a(x))[3]
+	# res <- rbind(res, c(t1,t2))
+# }
+# res <- cbind(N, res)
+# colnames(res) <- c("N", "old", "new")
+# res
+# save.image("kernel")
+
+# # first plot
+# plot(N[1:10], res[1:10,2], type="b", ylab="time (seconds)", xlab="sample size n", col=4, main="Timing comparison of old and new Pearson kernel functions")
+# points(N[1:10], res[1:10,3], type="b", col=2)
+# text(1000, 0.3, labels="new", col=2)
+# text(965, 9.2, labels="old", col=4)
+
+# # second plot
+# plot(N[10:19], res[10:19,2], type="b", ylab="time (seconds)", xlab="sample size n", col=4, main="Timing comparison of old and new Pearson kernel functions")
+# points(N[10:19], res[10:19,3], type="b", col=2)
+# text(10000, 100, labels="new", col=2)
+# text(9650, 3500, labels="old", col=4)
+
+
