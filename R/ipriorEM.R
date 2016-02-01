353a51a948bbc5f45897e18ea289c01f8d862048
Haziq Jamil <h.jamil@lse.ac.uk>
2016-02-01 00:34:55 +0000

    Fixed #11 - Detailed progress feedback
    The programme has been updated to include the predicted log-likelihood
    feature. It is built directly into the EM feedback report when running
    `iprior`. There is also a separate function to manipulate this table.
    
    More info on this feature when I merge the branch to master.

--- R/ipriorEM.R
+++ R/ipriorEM.R
@@ -2,7 +2,7 @@
 ### The iprior EM
 ###
 
-ipriorEM <- function(x, y, whichkernel, interactions, one.lam, parsm, kernel, maxit, stop.crit, report.int, silent, alpha.init, lambda.init, psi.init, invmethod){
+ipriorEM <- function(x, y, whichkernel, interactions, one.lam, parsm, kernel, maxit, stop.crit, report.int, silent, alpha.init, lambda.init, psi.init, invmethod, clean){
 	### Library packages
 	require(Matrix, quietly=T)			#to create diagonal matrices
 	require(MASS, quietly=T)			#to sample from MVN dist.
@@ -43,6 +43,15 @@ ipriorEM <- function(x, y, whichkernel, interactions, one.lam, parsm, kernel, ma
 		else lambda <- lambda.init
 	}
 	psi <- psi.init
+	
+	### Store results
+	res.loglik <- as.data.frame(matrix(NA, nr=maxit+1, nc=3))	#loglik, predloglik, delta
+	res.param <- as.data.frame(matrix(NA, nr=maxit+1, nc=2+q))
+	res.loglik[min(100,maxit+1),] <- rnorm(3); res.param[min(100,maxit+1),] <- rnorm(2+q) #this is a cheat to correct the formatting of digits in table
+	rownames(res.loglik) <- paste0("Iteration ", 0:maxit, ":"); colnames(res.loglik) <- c("Log-likelihood", "Pred.log-lik.", "Delta(i,i-1)")
+	rownames(res.param) <- paste0("Iteration ", 0:maxit, ":")
+	if(q == 1) colnames(res.param) <- c("(Intercept)", "lambda", "psi")	
+	else colnames(res.param) <- c("(Intercept)", paste0("lambda", 1:q), "psi")
 	
 	### Define the kernel matrix
 	H.mat <- NULL; H.matsq <- NULL
@@ -102,10 +111,19 @@ ipriorEM <- function(x, y, whichkernel, interactions, one.lam, parsm, kernel, ma
 	Var.Y <- psi*H.mat.lamsq + (1/psi)*diag(N)
 	ifelse(invmethod == "eigen", Var.Y.inv <- solve(Var.Y), Var.Y.inv <- chol2inv(chol(Var.Y)) )
 	log.lik0 <- dmvn(Y, rep(alpha,N), Var.Y, log=T)
-	
-	if(!silent && report.int != maxit) cat("Iteration 0:\t Log-likelihood =", round(log.lik0, 4), " ")
-	if(!silent) pb <- txtProgressBar(min=0, max=report.int*10, style=1, char=".") #progress bar
+
 	log.lik1 <- log.lik0 + 2*stop.crit
+	res.loglik[1,1] <- log.lik1#; res.loglik[min(100,maxit+1),] <- rep(log.lik1,3)
+	res.param[1,] <- c(alpha, lambda, psi)
+	if(!silent){
+		if(clean) cat("Iteration 0:\t Log-likelihood =", log.lik0, " ")
+		else{
+			tab.tmp <- cbind(res.loglik, res.param[,-1])[c(1,min(100,maxit+1)),]
+			cat(capture.output(tab.tmp)[1])
+			cat("\n", capture.output(tab.tmp)[2], " ", sep="")
+		}
+	}
+	if(!silent) pb <- txtProgressBar(min=0, max=report.int*10, style=1, char=".") #progress bar
 	i <- 0
 	
 	while((i != maxit) && (abs(log.lik0 - log.lik1) > stop.crit)){
@@ -154,20 +172,39 @@ ipriorEM <- function(x, y, whichkernel, interactions, one.lam, parsm, kernel, ma
 		ifelse(invmethod == "eigen", Var.Y.inv <- solve(Var.Y), Var.Y.inv <- chol2inv(chol(Var.Y)) )
 		log.lik1 <- dmvn(Y, rep(alpha,N), Var.Y, log=T)
 		
+		### Storage
+		dloglik <- log.lik1-log.lik0
+		dloglikold <- res.loglik[i,3]
+		a <- ifelse((0 < dloglik) & (dloglik < dloglikold), dloglik/dloglikold, 0)
+		predloglik <- log.lik1 - dloglik + dloglik/(1-a)
+		res.loglik[i+1,] <- c(log.lik1, predloglik, dloglik)	
+		res.param[i+1,] <- c(alpha, lambda, psi)
+		
 		### Report
 		check <- i %% report.int
 		if(log.lik1 < log.lik0) warning(paste("Log-likelihood decreased at iteration", i), call.=F)
-		if(!is.na(check) && check==0 && !silent) cat("\nIteration", paste0(i, ":"), "\t Log-likelihood =", round(log.lik1, 4), " ")  
-		if(!silent && report.int != maxit) setTxtProgressBar(pb, i)		
+		if(!is.na(check) && check==0 && !silent){
+			if(clean) cat("\nIteration", paste0(i, ":"), "\t Log-likelihood =", log.lik1, " ")
+			else cat("\n", capture.output( cbind(res.loglik, res.param[,-1])[i:(i+1),])[3], " ", sep="")
+		}
+		if(!silent) setTxtProgressBar(pb, i)		
 		if(i %% report.int*10 == 0 && !silent) pb <- txtProgressBar(min=i, max=report.int*10+i, style=1, char=".") 
 			#reset progress bar
 	}
 	
-	if(!silent && report.int != maxit) close(pb)
+	### Final report
+	if(!silent && check!=0){
+		if(clean) cat("\nIteration", paste0(i, ":"), "\t Log-likelihood =", log.lik1, " ")
+		else cat("\n", capture.output( cbind(res.loglik, res.param[,-1])[i:(i+1),])[3], " ", sep="")
+	}
+	
+	res.loglik <- res.loglik[1:(i+1),]; res.param <- res.param[1:(i+1),]
+	
+	if(!silent) close(pb)
 	converged <- !(abs(log.lik0 - log.lik1) > stop.crit)
 	if(!silent && converged) cat("EM complete.\n", "\nNumber of iterations =", i, "\n")
 	else if(!silent) cat("EM NOT CONVERGED!\n", "\nNumber of iterations =", i, "\n")
 	if(!silent) cat("Log-likelihood = ", log.lik1, "\n")
 	
-	list(alpha=alpha, lambda=lambda, psi=psi, log.lik=log.lik1, no.iter=i, H.mat=H.mat, H.matsq=H.matsq, H.mat.lam=H.mat.lam, VarY=Var.Y, w.hat=w.hat, kernel=kernel, whichPearson=whichkernel, converged=converged, stop.crit=stop.crit, one.lam=one.lam, parsm=parsm, interactions=interactions, q=q)
+	list(alpha=alpha, lambda=lambda, psi=psi, log.lik=log.lik1, no.iter=i, H.mat=H.mat, H.matsq=H.matsq, H.mat.lam=H.mat.lam, VarY=Var.Y, w.hat=w.hat, kernel=kernel, whichPearson=whichkernel, converged=converged, stop.crit=stop.crit, one.lam=one.lam, parsm=parsm, interactions=interactions, q=q, res.loglik=res.loglik, res.param=res.param)
 }
