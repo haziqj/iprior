63be05e4af4e1b2509f1122a1d7bf4d0cac4f9e6
Haziq Jamil <h.jamil@lse.ac.uk>
2016-06-28 19:29:32 +0100

    Updates to test functions

--- tests/testthat/test-iprior.R
+++ tests/testthat/test-iprior.R
@@ -1,17 +1,18 @@
 context("model fitting")
 
 test_that("Fitted object is iprior",{
-	
+
 	x <- rnorm(100); y <- rnorm(100)
-	mod <- iprior(x=x, y=y, silent=T)
+	mod <- iprior(y=y, x=x, control=list(silent=T))
 	expect_that(mod, is_a("iprior"))
-	
+
 })
 
 test_that("Successfully fit interactions",{
-	
+
 	data(cats, package="MASS")
-	mod <- iprior(Hwt ~ Bwt * Sex, cats, silent=T)
-	expect_that(mod, is_a("iprior"))
-	
+	mod1 <- iprior(Hwt ~ Bwt * Sex, cats, control=list(silent=T))
+	mod2 <- iprior(y=cats$Hwt, Bwt=cats$Bwt, Sex=cats$Sex, model=list(interactions="1:2"), control=list(silent=T))
+	expect_equal(mod1$log, mod2$log) #check if the same log-likelihood value achieved
+
 })
--- tests/testthat/test-kernel.R
+++ tests/testthat/test-kernel.R
@@ -1,14 +1,16 @@
 context("kernel matrices")
 
 test_that("FBM with Hurst=1 equals Canonical",{
-	
+
 	x <- rnorm(100)
-	expect_equal(fn.H3a(x, gamma=1), fn.H2a(x))
-	
+	mat.fbm <- fn.H3a(x, gamma=1); attr(mat.fbm, "class") <- NULL
+	mat.can <- fn.H2a(x); attr(mat.can, "class") <- NULL
+	expect_equivalent(mat.fbm, mat.can)
+
 })
 
 test_that("Pearson must take in factors",{
-	
+
 	expect_warning(fn.H1(1:100))
-	
+
 })
