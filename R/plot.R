9553e110bbc19c6b8460ead44ca1974259444a35
Haziq Jamil <h.jamil@lse.ac.uk>
2016-07-04 04:46:56 +0100

    Documentation for plot.R

--- R/plot.R
+++ R/plot.R
@@ -1,14 +1,49 @@
+#' Plots for \code{ipriorMod} objects
+#'
+#' Three plots are produced by default: Plot of fitted regression curve, plot of
+#' fitted values against residuals, and a QQ-plot of the residuals. Note that
+#' The plots of fitted regression line can be shown only if the explanatory
+#' variable is of dimension one (i.e. only \code{p = 1} explanatory variable
+#' used).
+#'
+#' @param x An object of class \code{ipriorMod}.
+#' @param plots Option to control which plots to show. The options are:
+#'   \describe{\item{\code{all}}{(default) All three plots are
+#'   shown.}\item{\code{allinone}}{All three plots are shown one one
+#'   screen.}\item{\code{fitted}}{Only the fitted regression curve is
+#'   shown.}\item{\code{diagnostic}}{The two diagnostic plots are
+#'   shown.}\item{\code{residuals}}{Only the plot of fitted against residuals
+#'   shown.}\item{\code{qqplot}}{Only the QQ-plot of the residuals is shown.}}
+#' @param own.labels Logical, useful when categorical variables has factor names
+#'   which are long, because these are used as the points of the plots.
+#' @param ... No further arguments are passed, so this is not used here.
+#'
+#' @examples
+#' # Straight line regression (Canonical RKHS)
+#' mod.orange <- iprior(circumference ~ age, Orange)
+#' plot(mod.orange)
+#'
+#' # Multilevel model type plots (Canonical + Pearson RKHS)
+#' mod.tooth1 <- iprior(len ~ ., ToothGrowth)
+#' mod.tooth2 <- iprior(len ~ . ^ 2, ToothGrowth)
+#' par(mfrow = c(1,2))
+#' plot(mod.tooth1, plots = "fitted")  # random intercept
+#' plot(mod.tooth2, plots = "fitted")  # random slopes & intercept
+#'
+#' # One-dimensional smoothing (FBM RKHS with Hurst coef. 0.5)
+#' mod.cars <- iprior(dist ~ speed, cars, model = list(kernel = "FBM"))
+#' plot(mod.cars, plots = "fitted")
+#'
+#' @name plot
 #' @export
 plot.ipriorMod <- function(x,
                            plots = c("all", "allinone", "fitted", "diagnostic",
                                      "residuals", "qqplot"),
-                           UseOwnLabels = FALSE, ...) {
+                           own.labels = FALSE, ...) {
   object <- x
 	x <- object$ipriorKernel$x
 	y <- object$ipriorKernel$Y
-	p <- object$ipriorKernel$p
 	whichPearson <- isPea(object$ipriorKernel$model$kernel)
-	no.plot <- sum(!whichPearson)
 	xnames <- object$ipriorKernel$model$xname
 	yname <- object$ipriorKernel$model$yname
 	yhat <- object$fitted
@@ -23,7 +58,7 @@ plot.ipriorMod <- function(x,
 	          RColorBrewer::brewer.pal(8, "Dark2"))
 	colx[6] <- RColorBrewer::brewer.pal(8, "Set2")[6]
 
-	if(!is.numeric(plots)){
+	if (!is.numeric(plots)) {
 		thisplot <- match.arg(plots)
 		if ((thisplot == "all") | (thisplot == "allinone")) thisplot <- 1:3
 		else if (thisplot == "fitted") thisplot <- c(1)
@@ -33,58 +68,60 @@ plot.ipriorMod <- function(x,
 	}
 	else{
 		thisplot <- plots
-		if (any((thisplot > 3) | (thisplot < 1))) stop("Must be between 0 and 1.")
+		if (any((thisplot > 3) | (thisplot < 1))) stop("Must be either 1, 2 or 3.")
 	}
 	whichplot <- (1:3) %in% thisplot
 
-	### Plot 1: Fitted values
-	cts.vars <- which(!whichPearson)
-	ctg.vars <- which(whichPearson)
+	# Plot 1: Fitted regression curve --------------------------------------------
+	which.cts <- which(!whichPearson)
+	which.ctg <- which(whichPearson)
 
-	if ((length(cts.vars) == 1) | (length(ctg.vars) > 0)) {
-		if (length(ctg.vars) == 0) {
+	if ((length(which.cts) == 1) | (length(which.ctg) > 0)) {
+	  x.cts <- unlist(x[which.cts], use.names = FALSE)
+	  if (length(x.cts) != length(y)) stop("X variable more than 1 dimensions.")
+	  if (length(which.ctg) == 0) {
 		  # Plot only continuous variables -----------------------------------------
-			x.cts <- unlist(x[cts.vars], use.names=F)
-			if (length(x.cts) != length(y)) stop("X variable not a vector.")
-			plot1 <- function(z){
+			plot1 <- function(z) {
 				xorder <- order(x.cts)
-				plot(x=x.cts, y=y, xlab=xnames[cts.vars], ylab=yname, main="Fitted regression curve", cex=0.55)
-				lines(x=x.cts[xorder], y=yhat[xorder], col=colx[1], lwd=1.55)
+				plot(x = x.cts, y = y, xlab = xnames[which.cts], ylab = yname,
+				     main = "Fitted regression curve", cex = 0.55)
+				lines(x = x.cts[xorder], y = yhat[xorder], col = colx[1], lwd = 1.55)
 			}
 		} else {
 		  # Define categorical variables -------------------------------------------
-		  if (length(ctg.vars) == 1) x.ctg <- as.factor(x[[ctg.vars]])
+		  if (length(which.ctg) == 1) x.ctg <- as.factor(x[[which.ctg]])
 			else {
-				x.ctg <- as.data.frame(x[ctg.vars])
+				x.ctg <- as.data.frame(x[which.ctg])
 				x.ctg <- interaction(x.ctg)
 				# x.ctg <- x.ctg[,2]
 			}
-			if (length(cts.vars) == 0) {
+			if (length(which.cts) == 0) {
 			  # Plot only categorical variables --------------------------------------
 				yhat.unq <- unique(yhat)
 				plotlvl <- levels(x.ctg)
 				grp <- as.numeric(x.ctg)
 				plotlvl <- plotlvl[unique(grp)]
 				grp <- as.numeric(factor(grp))
-				if (UseOwnLabels) plotlvl <- unique(grp)
-				plot1 <- function (z) {
-					plot(x=grp, y=y, type="n", xlab=xnames[ctg.vars], ylab=yname, main="Fitted regression curve", xaxt="n", xlim=c(0.5, length(unique(grp))+0.5))
+				if (own.labels) plotlvl <- unique(grp)
+				plot1 <- function(z) {
+					plot(x = grp, y = y, type = "n", xlab = xnames[which.ctg],
+					     ylab = yname, main = "Fitted regression curve", xaxt = "n",
+					     xlim = c(0.5, length(unique(grp)) + 0.5))
 					for (i in unique(grp)) {
-						text(x=grp[grp==i], y[grp==i], plotlvl[i], col=colx[(i-1)%%16+1], cex=0.8)
-						abline(a=yhat.unq[i], b=0, col=colx[(i-1)%%16+1])
+						text(x = grp[grp == i], y[grp == i], plotlvl[i],
+						     col = colx[(i - 1) %% 16 + 1], cex = 0.8)
+						abline(a = yhat.unq[i], b = 0, col = colx[(i - 1) %% 16 + 1])
 					}
 				}
 			} else {
 				# Multilevel plots -----------------------------------------------------
-			  x.cts <- as.numeric(x[[cts.vars]])
-				if (length(x.cts) != length(y)) stop("X variable not a vector.")
 				plotlvl <- levels(x.ctg)
 				grp <- as.numeric(x.ctg)
 				plotlvl <- plotlvl[unique(grp)]
 				grp <- as.numeric(factor(grp))
-				if (UseOwnLabels) plotlvl <- unique(grp)
+				if (own.labels) plotlvl <- unique(grp)
 				plot1 <- function(z) {
-					plot(x = x.cts, y = y, type = "n", xlab = xnames[cts.vars],
+					plot(x = x.cts, y = y, type = "n", xlab = xnames[which.cts],
 					     ylab = yname, main = "Fitted regression curve")
 					for (i in unique(grp)) {
 						xorder <- order(x.cts[grp == i])
@@ -98,40 +135,40 @@ plot.ipriorMod <- function(x,
 		}
 	} else {
 		whichplot[1] <- FALSE
-		message("Fitted plot not generated because number of continuous x variables is greater than 1.")
+		message("Fitted plot not generated because dimensions of continuous x variables is greater than 1.")
 	}
 
-	### Plot 2: Fitted vs. Residuals
+	# Plot 2: Fitted vs. Residuals -----------------------------------------------
 	plot2 <- function(z){
-		plot(x=yhat, y=resid, xlab="Fitted values", ylab="Residuals", main="Fitted vs. Residuals")
-		text(yhat[top3], resid[top3], as.character(top3), pos=4, cex=0.8)
-		abline(0, 0, col=200, lty=3, lwd=1.5)
+		plot(x = yhat, y = resid, xlab = "Fitted values", ylab = "Residuals",
+		     main = "Fitted vs. Residuals")
+		text(yhat[top3], resid[top3], as.character(top3), pos = 4, cex = 0.8)
+		abline(0, 0, col = 200, lty = 3, lwd = 1.5)
 	}
 
-	### Plot 3: Normal Q-Q plot
+	# Plot 3: Normal Q-Q plot ----------------------------------------------------
 	plot3 <- function(z){
-		tmp <- qqnorm(scale(resid), ylab="Standardised Residuals")
+		tmp <- qqnorm(scale(resid), ylab = "Standardised Residuals")
 		updown <- c(3,3,3); updown[which(tmp$y[top3,] > 0)] <- 1
-		text(tmp$x[top3], tmp$y[top3,], as.character(top3), pos=updown, cex=0.8)
-		abline(0, 1, col=200, lty=3, lwd=1.5)
+		text(tmp$x[top3], tmp$y[top3, ], as.character(top3), pos = updown, cex = 0.8)
+		abline(0, 1, col = 200, lty = 3, lwd = 1.5)
 	}
 
-	## Plot
-	if(!is.numeric(plots) && match.arg(plots) == "allinone"){
-		dev.new(width=14, height=5.3)
-		par(mfrow=c(1,3))
-		for(i in which(whichplot)){
+	# Plot the plots -------------------------------------------------------------
+	if (!is.numeric(plots) && match.arg(plots) == "allinone") {
+		dev.new(width = 14, height = 5.3)
+		par(mfrow = c(1,3))
+		for (i in which(whichplot)) {
 			do.call(paste0("plot", i), list(i))
 		}
 	}
 	else{
 		timetostop <- length(which(whichplot))
 		tts.count <- 1
-		for(i in which(whichplot)){
+		for (i in which(whichplot)) {
 			do.call(paste0("plot", i), list(i))
-			if(!(tts.count == timetostop)) readline("Hit <Return> to see next plot")
+			if (!(tts.count == timetostop)) readline("Hit <Return> to see next plot")
 			tts.count <- tts.count + 1
 		}
 	}
-
 }
