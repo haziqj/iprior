6e53f22ac29399c12b25c2d17512018ef9646caf
Haziq Jamil <h.jamil@lse.ac.uk>
2017-09-20 19:38:41 +0100

    Amendment to tests. Corrected values.

--- tests/testthat/test-fitted_and_predict.R
+++ tests/testthat/test-fitted_and_predict.R
@@ -8,8 +8,8 @@ test_that("Fitted", {
   mod <- iprior2(y, x1)
   tmp <- fitted(mod, intervals = TRUE)
   expect_equal(tmp$y, c(-0.4288420, -0.3579418, 1.5548390), tolerance = 1e-6)
-  expect_equal(tmp$lower, c(-0.4677753, -0.3956874, 1.5001975), tolerance = 1e-6)
-  expect_equal(tmp$upper, c(-0.3899087, -0.3201962, 1.6094805), tolerance = 1e-6)
+  expect_equal(tmp$lower, c(-0.7050808, -0.6299344, 1.2275848), tolerance = 1e-6)
+  expect_equal(tmp$upper, c(-0.15260325, -0.08594914, 1.88209317), tolerance = 1e-6)
   expect_that(print(tmp), prints_text("Training MSE:"))
 
 })
@@ -23,8 +23,8 @@ test_that("Predict (non-formula)", {
   tmp <- predict(mod, list(rnorm(1)), y.test = rnorm(1), intervals = TRUE)
 
   expect_equal(tmp$y, -1.342379, tolerance = 1e-6)
-  expect_equal(tmp$lower, -1.408215, tolerance = 1e-6)
-  expect_equal(tmp$upper, -1.276543, tolerance = 1e-6)
+  expect_equal(tmp$lower, -1.701595, tolerance = 1e-6)
+  expect_equal(tmp$upper, -0.9831631, tolerance = 1e-6)
   expect_that(print(tmp), prints_text("Test MSE:"))
   expect_that(print(predict(mod, list(rnorm(1)))), prints_text("Test MSE: NA"))
 
@@ -37,8 +37,8 @@ test_that("Predict (formula)", {
   tmp <- predict(mod, dat[4, ], intervals = TRUE)
 
   expect_equal(as.numeric(tmp$y), 17.30887, tolerance = 1e-6)
-  expect_equal(as.numeric(tmp$lower), 13.09142, tolerance = 1e-6)
-  expect_equal(as.numeric(tmp$upper), 21.52631, tolerance = 1e-6)
+  expect_equal(as.numeric(tmp$lower), 14.43379, tolerance = 1e-6)
+  expect_equal(as.numeric(tmp$upper), 20.18394, tolerance = 1e-6)
   expect_that(print(tmp), prints_text("Test MSE:"))
 
 })
