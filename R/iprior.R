54d8eea0506601b80f2d1b1ccb7831300ba9dd8d
Haziq Jamil <h.jamil@lse.ac.uk>
2018-10-16 07:43:29 +0800

    Fixed CRAN ERROR message for R-devel.
    In the numeric method for all.equal(), a numeric scale argument is now checked to be positive and allowed to be of length > 1.

--- R/iprior.R
+++ R/iprior.R
@@ -119,7 +119,7 @@ iprior <- function(...) UseMethod("iprior")
 #' mod5 <- iprior(y ~ X, dat, method = "fixed", lambda = coef(mod1)[1],
 #'                psi = coef(mod1)[2])
 #' all.equal(logLik(mod1), logLik(mod2), logLik(mod3), logLik(mod4),
-#'           logLik(mod5), tol = 1e-6)
+#'           logLik(mod5), tol = 1e-6, scale = 1)
 #'
 #' \dontrun{
 #'
