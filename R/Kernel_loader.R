64c404406034d738158bfb437ac416e62225703b
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2022-11-06 08:51:46 +0800

    Tweak to nystrom samples
    Use a uniform interval instead

--- R/Kernel_loader.R
+++ R/Kernel_loader.R
@@ -198,8 +198,13 @@ kernL.default <- function(y, ..., kernel = "linear", interactions = NULL,
   if (as.numeric(nystrom) > 0 & as.numeric(nystrom) != n) {
     if (as.numeric(nystrom) == 1) nystrom <- floor(0.1 * n)
     if (!is.null(nys.seed)) set.seed(nys.seed)
-    nys.samp <- sample(seq_along(y))
+    # nys.samp <- sample(seq_along(y))
+    nys.samp <- floor(seq(1, length(y), length = nystrom))
+    # y.tmp <- y[nys.samp]
+    y.tmp <- sort(y)[nys.samp]
+    nys.samp <- which(y == y.tmp)
     y.tmp <- y[nys.samp]
+    
     mostattributes(y.tmp) <- attributes(y)
     y <- y.tmp
     tmp <- lapply(Xl, reorder_x, smp = nys.samp)
